92e366c69fd9811b5df38c1265e499cc110f2820
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-07-29 18:24:22 +0700

    Spring setup Создана таблица tag

---
+++ src/main/resources/db/migration/V1__creating tag tаble.sql
@@ -0,0 +1,17 @@
+create table tag
+(
+    tag_id   bigserial
+        constraint tag_pk
+            primary key,
+    tag_name text not null
+);
+
+alter table tag
+    owner to postgres;
+
+create unique index tag_tag_id_uindex
+    on tag (tag_id);
+
+create unique index tag_tag_name_uindex
+    on tag (tag_name);
+
