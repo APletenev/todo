83728b7fd1ded549c3aae9ff445131927e9ca4f5
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-08-01 14:51:27 +0700

    Возможность вернуть список всех задач (GET: /tasks)

--- src/main/java/com/example/todo/controller/TaskController.java
+++ src/main/java/com/example/todo/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.example.todo.controller;
 import com.example.todo.model.Task;
 import com.example.todo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class TaskController {
     @PostMapping("/task") //создать запись
     public Task create(@RequestBody Task task) {
         return taskRepository.save(task);
+    }
+
+    @GetMapping("/tasks") //получить список всех задач
+    public Iterable<Task> get() {//получить все записи
+        return taskRepository.findAll();
     }
 }
