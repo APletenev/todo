66d5f2312986724a453420b89ee4f106f0c2d452
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-10-09 15:49:55 +0700

    Сделано понятное сообщение об ошибке при попытке обращения к несуществующему ресурсу.

--- src/main/java/com/example/todo/handler/RestExceptionHandler.java
+++ src/main/java/com/example/todo/handler/RestExceptionHandler.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -14,12 +16,17 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ValidationErrorResponse onMethodArgumentNotValidException(
-            MethodArgumentNotValidException e
-    ) {
+    public ValidationErrorResponse onMethodArgumentNotValidException (MethodArgumentNotValidException e) {
         final List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
                 .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
                 .collect(Collectors.toList());
         return new ValidationErrorResponse(violations);
+    }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> onIllegalArgumentException (IllegalArgumentException e) {
+        return Collections.singletonMap("Error", e.getLocalizedMessage());
     }
 }
