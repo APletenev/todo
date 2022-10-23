85030a86fd5d080180fae2cda51aabd492fd4ab2
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-10-23 21:38:22 +0700

    Настроены фильтры Spring Security и использование учетных данных из базы.

--- src/main/java/com/example/todo/security/SecurityConfig.java
+++ src/main/java/com/example/todo/security/SecurityConfig.java
@@ -1,17 +1,39 @@
 package com.example.todo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .antMatchers("/tag/**").permitAll()
-                .antMatchers("/tasks").permitAll()
-                .and().formLogin().permitAll();
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
+
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET,"/tag/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/tasks").permitAll()
+                .antMatchers("/**").hasRole("EDITOR")
+                .and().formLogin().permitAll()
+                .and().httpBasic();
+        return http.build();
     }
 }
