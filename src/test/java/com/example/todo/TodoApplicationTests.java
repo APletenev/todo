32a7362620b14f6cb32089942d3b53c3d8a1aa24
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-12-17 11:45:07 +0700

    Улучшение диагностики интеграционного теста

--- src/test/java/com/example/todo/TodoApplicationTests.java
+++ src/test/java/com/example/todo/TodoApplicationTests.java
@@ -12,13 +12,18 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "36000")
 @ActiveProfiles("nosecurity_test")
 class TodoApplicationTests {
+
+    public static final String RESET = "\033[0m";
+
+    public static final String BLUE = "\033[0;34m";
+
     @Autowired
     private WebTestClient webTestClient;
 
@@ -86,23 +91,23 @@ class TodoApplicationTests {
     }
 
 
-
     @Test
     void runIntegrationTests() throws Exception {
 
-//      Проверяем, что имеем дело с пустой базой и контроллер возвращает пустой список задач
+        System.out.println(BLUE + "Проверяем, что имеем дело с пустой базой и контроллер возвращает пустой список задач"+RESET);
         checkNoTasks();
 
 
-        // Создаем тег1
+        System.out.println(BLUE + "Создаем тег1"+RESET);
+
         Tag tag1 = new Tag("home");
         tag1 = createTagTest(tag1);
 
-        // Создаем тег2
+        System.out.println(BLUE + "Создаем тег2"+RESET);
         Tag tag2 = new Tag("work");
         tag2 = createTagTest(tag2);
 
-        // Проверить, что нельзя создать задачу без имени
+        System.out.println(BLUE + "Проверить, что нельзя создать задачу без имени"+RESET);
         Task task0 = new Task("", "Задача без имени", LocalDate.now(), tag1.getTag_id());
         webTestClient
                 .post()
@@ -114,46 +119,45 @@ class TodoApplicationTests {
                 .expectBody(Task.class)
                 .consumeWith(System.out::println);
 
-        // Создаем задачу 1 с привязкой к тегу 1
+        System.out.println(BLUE + "Создаем задачу 1 с привязкой к тегу 1"+RESET);
         Task task1 = new Task("Задача 1", "Описание задачи 1", LocalDate.now(), tag1.getTag_id());
         task1 = createTaskTest(task1);
 
-        // Создаем задачу 2 с привязкой к тегу 2
+        System.out.println(BLUE + "Создаем задачу 2 с привязкой к тегу 2"+RESET);
         Task task2 = new Task("Задача 2", "Описание задачи 2", LocalDate.now(), tag2.getTag_id());
         task2 = createTaskTest(task2);
 
-        // Создаем задачу 3 с привязкой к тегу 2
+        System.out.println(BLUE + "Создаем задачу 3 с привязкой к тегу 2"+RESET);
         Task task3 = new Task("Задача 3", "Описание задачи 3", LocalDate.now(), tag2.getTag_id());
         task3 = createTaskTest(task3);
 
-//      Проверяем, что контроллер возвращает список из 3 задач
+        System.out.println(BLUE + "Проверяем, что контроллер возвращает список из 3 задач"+RESET);
         checkTasksCount(3);
 
-//      Изменяем имя второй задачи
+        System.out.println(BLUE + "Изменяем имя второй задачи"+RESET);
         task2.setTask_name("Новая задача 2");
         Task newtask2 = createTaskTest(task2);
 
-//      Проверяем что имя второй задачи изменилось
+        System.out.println(BLUE + "Проверяем что имя второй задачи изменилось"+RESET);
         assertEquals(task2.getTask_name(), newtask2.getTask_name());
 
-//      Проверяем что ID второй задачи не изменился
+        System.out.println(BLUE + "Проверяем что ID второй задачи не изменился"+RESET);
         assertEquals(task2.getTask_id(), newtask2.getTask_id());
 
-//      Проверяем, что количество задач не изменилось
+        System.out.println(BLUE + "Проверяем, что количество задач не изменилось"+RESET);
         checkTasksCount(3);
 
-//      Меняем название тега2
+        System.out.println(BLUE + "Меняем название тега2"+RESET);
         tag2.setTag_name("business");
         Tag newtag2 = createTagTest(tag2);
 
-//      Проверяем что ID второго тега не изменился
+        System.out.println(BLUE + "Проверяем что ID второго тега не изменился"+RESET);
         assertEquals(tag2.getTag_id(), newtag2.getTag_id());
 
-
-//      Получаем тег2 по УИД и все его задачи
+        System.out.println(BLUE + "Получаем тег2 по УИД и все его задачи"+RESET);
         tag2 = webTestClient
                 .get()
-                .uri("/tag/"+newtag2.getTag_id())
+                .uri("/tag/" + newtag2.getTag_id())
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
@@ -161,36 +165,35 @@ class TodoApplicationTests {
                 .consumeWith(System.out::println)
                 .returnResult().getResponseBody();
 
-//      Проверяем, что у тега2 есть 2 задачи
+        System.out.println(BLUE + "Проверяем, что у тега2 есть 2 задачи"+RESET);
         assertEquals(tag2.getTasks().size(), 2);
 
-//      Проверяем задачи тега
-        assertEquals(tag2.getTasks().get(0), newtask2);
-        assertEquals(tag2.getTasks().get(1), task3);
+        System.out.println(BLUE + "Проверяем задачи тега"+RESET);
+        assertTrue(tag2.getTasks().contains(newtask2));
+        assertTrue(tag2.getTasks().contains(task3));
 
-
-//      Проверяем возможность каскадно удалить тег со всеми прикрепленными к нему задачами
+        System.out.println(BLUE + "Проверяем возможность каскадно удалить тег со всеми прикрепленными к нему задачами"+RESET);
         webTestClient
                 .delete()
-                .uri("/tag/"+newtag2.getTag_id())
+                .uri("/tag/" + newtag2.getTag_id())
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectBody()
                 .consumeWith(System.out::println);
 
-//      Убеждаемся, что осталась только одна задача
+        System.out.println(BLUE + "Убеждаемся, что осталась только одна задача"+RESET);
         checkTasksCount(1);
 
-//      Удаляем задачу по УИД
+        System.out.println(BLUE + "Удаляем задачу по УИД"+RESET);
         webTestClient
                 .delete()
-                .uri("/task/"+task1.getTask_id())
+                .uri("/task/" + task1.getTask_id())
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectBody()
                 .consumeWith(System.out::println);
 
-//      Проверяем, что больше нет ни одной задачи
+        System.out.println(BLUE + "Проверяем, что больше нет ни одной задачи"+RESET);
         checkNoTasks();
 
     }
